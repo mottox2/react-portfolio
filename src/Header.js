3ac1c9a30fd2cef245ad2557706530193127b243
Yuki Takemoto <mottox2@users.noreply.github.com>
2018-09-24 20:01:43 +0900

    Use styled-components

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+
+const Base = styled.header`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 72px;
+  width: 100%;
+  max-width: 630px;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  padding: 0 15px;
+  color: white;
+  font-family: "Poppins", sans-serif;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  letter-spacing: 1.5px;
+  font-weight: 600;
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+`;
+
+const NavItem = styled.a`
+  color: white;
+  font-size: 15px;
+  margin-left: 20px;
+  text-decoration: none;
+  font-weight: 600;
+  letter-spacing: 0.5px;
+`;
 
 const Header = () => {
   return (
-    <header className="header">
-      <h1 className="header-title">Portfolio</h1>
-      <nav className="headerNav">
-        <a className="headerNav-item" href="#about">
-          About
-        </a>
-        <a className="headerNav-item" href="#works">
-          Works
-        </a>
-      </nav>
-    </header>
+    <Base>
+      <Title>Portfolio</Title>
+      <Nav>
+        <NavItem href="#about">About</NavItem>
+        <NavItem href="#works">Works</NavItem>
+      </Nav>
+    </Base>
   );
 };
 
